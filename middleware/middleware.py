9c3f8955cd982a976393b9cf0b853252aae388c9
Santiago Vanegas <svaneg11@gmail.com>
2021-03-31 03:57:08 -0500

    "add consume functionality to api, add Dockerfile and docker-compose files, update requirements.txt"

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -2,11 +2,15 @@ import pika
 from database import *
 from auth import *
 from flask import Flask, request
-from flask_cors import CORS
+import logging
+
 
 RABBIT_HOST = '54.211.164.46'
 app = Flask(__name__)
-CORS(app)
+gunicorn_error_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers.extend(gunicorn_error_logger.handlers)
+app.logger.setLevel(logging.DEBUG)
+#CORS(app)
 connection = pika.BlockingConnection(pika.ConnectionParameters(RABBIT_HOST,
                                                                heartbeat=600,
                                                                blocked_connection_timeout=300))
@@ -32,7 +36,7 @@ def create_queue():
 
 @app.route('/list_queues', methods=['GET'])
 def list_queues():
-    return get_queues(), 200
+    return json.dumps(get_queues(), sort_keys=True, indent=4, separators=(',', ': ')), 200
 
 
 @app.route('/delete_queue', methods=['DELETE'])
@@ -98,6 +102,50 @@ def register():
         return 'Username is taken', 403
     else:
         return 'User has been registered successfully', 200
+
+
+@app.route('/consume', methods=['POST'])
+def consume():
+    body = request.get_json(force=True)
+    queue_name = body.get('queue_name', '')
+    timeout = body.get('timeout', 5)
+    limit = body.get('limit', 10)
+
+
+    queues = get_queues()
+    queue_exist = False
+    for q in queues:
+        if q['queue_name'] == queue_name:
+            queue_exist = True
+
+    if not queue_name or not queue_exist:
+        return 'Invalid queue_name', 400
+
+    chnl = connection.channel()
+    messages = []
+    chnl.queue_declare(queue_name)
+    for method_frame, properties, bdy in chnl.consume(queue_name, inactivity_timeout=timeout):
+        if bdy:
+            messages.append(str(bdy.decode('utf-8')))
+
+        # Acknowledge the message
+        try:
+            chnl.basic_ack(method_frame.delivery_tag)
+        except Exception as e:
+            break
+
+        # Escape out of the loop after 10 messages
+        if method_frame.delivery_tag == limit:
+            break
+
+    # Cancel the consumer and return any pending messages
+    requeued_messages = chnl.cancel()
+
+    print('Requeued %i messages' % requeued_messages)
+    # Close the chnl and the connection
+    chnl.close()
+
+    return str(messages), 200
 
 
 if __name__ == '__main__':
